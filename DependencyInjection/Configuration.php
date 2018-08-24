778c3835fa722cdd577181f350371ad5187f7ea7
Oskar Gunther <o.gunther@consultia.pl>
2018-08-24 15:28:08 +0200

    Init

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OG\SendGridBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('og_send_grid')->children()
+            ->scalarNode('api_key')
+                ->isRequired()
+            ->end()
+            ->booleanNode('disable_delivery')
+                ->defaultFalse()
+            ->end()
+        ->end();
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
---
+++ OGSendGridBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OG\SendGridBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OGSendGridBundle extends Bundle
+{
+}
